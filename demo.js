f2d6bc12f264527b0067f2b7ca600266868ea6d1
Nathan Herald <me@nathanherald.com>
2023-09-17 16:50:41 +0200

    Support manual slot assignment

--- demo.js
+++ demo.js
@@ -45,6 +45,10 @@ const voidElementNames = [
   'wbr'
 ]
 
+// TODO: async generator that somehow knows when and how to give custom
+// elements enough time to fully setup
+
+// generator function to support streaming responses
 function *serializer (node, opts = {}) {
   const closedRoots = opts.closedRoots || []
   let closedRootsMap = new WeakMap()
@@ -66,41 +70,72 @@ function *serializer (node, opts = {}) {
     }
   }
 
+  // This is very naive
   const attributesArray = Array.from(attributes.entries()).map(([k,v]) => `${k}="${v}"`)
   const attributesString = attributesArray.length > 0 ? ` ${attributesArray.join(' ')}` : ''
 
+  // yield this node's tag name + attributes
+  // This probably only works for simple HTML5 and I'm OK with that for now
   yield `<${tagName}${attributesString}>`
 
-  function *serializeChildren(parent) {
-
+  function *serializeChildrenOfNode(parent) {
     // find it's first child
     let child = parent.firstChild
 
     // serialize each of the children one after the other
     while (child) {
-      if (child.nodeType === 1) {
-        yield* serializer(child, closedRootsMap)
-      } else if (child.nodeType === 3) {
-        yield child.nodeValue
-      } else {
-        throw new Error(`Don't support node type ${child.nodeType} yet`)
-      }
-
+      yield* serializeChild(child)
       child = child.nextSibling
     }
   }
 
-  yield* serializeChildren(node)
+  function *serializeAssignedNodesOfSlot(slot) {
+    for (const child of slot.assignedNodes()) {
+      yield* serializeChild(child)
+    }
+  }
 
-  // serialize this node's shadow root in a declarative way if it has one
+  function *serializeChild(child) {
+    if (child.nodeType === 1) {
+      yield* serializer(child, closedRootsMap)
+    } else if (child.nodeType === 3) {
+      yield child.nodeValue
+    } else {
+      throw new Error(`Don't support node type ${child.nodeType} yet`)
+    }
+  }
+
+  // Does this node have a shadow root or is a closed root we want to serailize
+  // down through?
   const shadow = node.shadowRoot || closedRootsMap.get(node)
 
   if (shadow) {
+    // There is a complex scenario where the slot assignment is manual. We
+    // can't express that declaratively today. So to approximate the current
+    // dom, we must only serialize what is currently slotted out as the light
+    // dom, which might be a subset of the actual light dom.
+
+    // yield the light dom elements before the shadow root's template
+    if (shadow.slotAssignment === 'manual') {
+      // Only yield the slotted light dom elements
+      for (const slot of shadow.querySelectorAll('slot')) {
+        yield* serializeAssignedNodesOfSlot(slot)
+      }
+    } else {
+      // yeild everything in the light dom
+      yield* serializeChildrenOfNode(node)
+    }
+
+    // yield everything currently inside the shadow root inside a declarative shadow root template
     yield `<template shadowrootmode="${shadow.mode}">\n`
-    yield* serializeChildren(shadow)
+    yield* serializeChildrenOfNode(shadow)
     yield `</template>\n`
+  } else {
+    // else just serialize the node without a template
+    yield* serializeChildrenOfNode(node)
   }
 
+  // html5!
   if (!(voidElementNames.includes(tagName))) {
     yield `</${tagName}>`
   }
